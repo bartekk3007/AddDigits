19c83a89a2bdcd7286f35b927105f1dc06b7a68d
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-03-10 22:02:28 +0100

    Add files via upload

---
+++ AddDigits/Solution.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+        if (num == 0)
+        {
+            return num;
+        }
+        else if (num % 9 == 0)
+        {
+            return 9;
+        }
+        else
+        {
+            return num % 9;
+        }
+    }
+};
---
+++ AddDigits/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Solution.hpp"
+
+int main()
+{
+	int num = 38;
+	Solution s1;
+	std::cout << s1.addDigits(num);
+
+	return 0;
+}
